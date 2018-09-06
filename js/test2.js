8407df57e1bced44af1795d3d9fb53adc23caf8c
Marwa Mostafa <marwa120640@gmail.com>
2018-09-06 21:40:02 +0200

    whole sections

--- js/test2.js
+++ js/test2.js
@@ -8,7 +8,27 @@ $(".option a").click(function(){
       $(".page-options").toggle(500); 
   }) 
 
+$("#acc div").css("display","none")
+
+$("#acc .li-1").click(function(){
+        $("#acc .ri-1").slideToggle(1000);
+})
+
+$("#acc .li-2").click(function(){
+        $("#acc .ri-2").slideToggle(1000);
+})
+
+$("#acc .li-3").click(function(){
+        $("#acc .ri-3").slideToggle(1000);
+})
+
+$("#acc .li-4").click(function(){
+        $("#acc .ri-4").slideToggle(1000);
+})
 
 
-
-
+var maxLength = 100;
+$('textarea').keyup(function() {
+  var textlen = maxLength - $(this).val().length;
+  $('#char').text(textlen);
+});
