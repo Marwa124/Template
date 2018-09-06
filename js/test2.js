5f9ecd20e9ab15e0d422c8538d09651f82cf1a57
Marwa Mostafa <marwa120640@gmail.com>
2018-09-06 19:36:55 +0200

    section-1

---
+++ js/test2.js
@@ -0,0 +1,14 @@
+$(".page-options").css("display","none")
+$(".option .st").click(function(){
+      $(".page-options").toggleClass(500);
+      
+  }) 
+$(".page-options").css("display","none")
+$(".option a").click(function(){
+      $(".page-options").toggle(500); 
+  }) 
+
+
+
+
+
